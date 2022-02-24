7f233a4de5f26309c4f244539ca531704f23cf39
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-24 07:07:31 -0500

    Finished SumOfInput exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
- * Created by iyasuwatts on 10/17/17.
+ * Prompts user to input a number and then prints the sum of numbers 1 - n.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Please enter a number: ");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++){
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to " + number + " is " + sum);
+
 
     }
 }
